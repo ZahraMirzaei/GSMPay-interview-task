499a5a7a4a3c371809f3bcfd90ef9a69a0e2645e
zahra mirzaei <39621660+ZahraMirzaei@users.noreply.github.com>
2024-01-16 19:01:20 +0330

    Revert "Feat/add new task box"

--- app/components/ui/buttons/index.ts
+++
@@ -1,4 +0,0 @@
-import { Button } from "@/app/components/ui/buttons/button";
-import { OutlineButton } from "@/app/components/ui/buttons/outline-button";
-
-export { Button, OutlineButton }
--- utils/convert-en-number-to-fa-number.ts
+++
@@ -1,7 +0,0 @@
-export function convertToPersianNumber(number: string) {
-  const persianDigits = "۰۱۲۳۴۵۶۷۸۹";
-  return String(number).replace(
-    /\d/g,
-    (digit: string) => persianDigits[Number(digit)]
-  );
-}
